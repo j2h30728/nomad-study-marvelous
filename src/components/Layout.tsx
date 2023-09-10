cd68185b56713e30fa8ef8f0b0344a0d69a3be50
Dam <60846068+j2h30728@users.noreply.github.com>
2023-09-11 05:09:09 +0900

    feat: 헤더 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,21 +6,21 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const isRootPath = pathname === ROUTE_PATH.ROOT;
 
   return (
-    <div className="w-full flex flex-col items-center bg-slate-200 h-[200vh] px-10">
-      <div className="text-5xl font-extrabold flex justify-between w-full p-10">
+    <div className="w-full flex flex-col items-center bg-slate-200 h-screen px-10">
+      <div className=" fixed bg-white  font-extrabold flex justify-between w-full p-8 shadow-xl">
         {isRootPath ? (
           <div></div>
         ) : (
-          <Link className="self-start" to={ROUTE_PATH.ROOT}>
+          <Link className="self-start text-2xl md:text-5xl sm:text-3xl" to={ROUTE_PATH.ROOT}>
             &lt;
           </Link>
         )}
-        <Link to={ROUTE_PATH.HOME} className="text-red-700">
+        <Link to={ROUTE_PATH.HOME} className="text-red-700 text-2xl md:text-5xl sm:text-3xl">
           MARVELOUS CHARACTER
         </Link>
         <div> </div>
       </div>
-      {children}
+      <div className="mt-32">{children}</div>
     </div>
   );
 };
